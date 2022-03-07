3fdbb27da02ec4d1802151cc563610442327cb56
Dekel Cohen <dekelcohen@gmail.com>
2022-03-07 20:33:29 +0200

    Refactor infer_one_sentence_proba

--- src/tasks/infer.py
+++ src/tasks/infer.py
@@ -186,25 +186,31 @@ class infer_from_trained(object):
         e1_e2_start = ([i for i, e in enumerate(x) if e == self.e1_id][0],\
                         [i for i, e in enumerate(x) if e == self.e2_id][0])
         return e1_e2_start
-    
-    def infer_one_sentence(self, sentence):
+
+    def infer_one_sentence_proba(self, sentence):
         self.net.eval()
-        tokenized = self.tokenizer.encode(sentence); #print(tokenized)
-        e1_e2_start = self.get_e1e2_start(tokenized); #print(e1_e2_start)
+        tokenized = self.tokenizer.encode(sentence);  # print(tokenized)
+        e1_e2_start = self.get_e1e2_start(tokenized);  # print(e1_e2_start)
         tokenized = torch.LongTensor(tokenized).unsqueeze(0)
         e1_e2_start = torch.LongTensor(e1_e2_start).unsqueeze(0)
         attention_mask = (tokenized != self.pad_id).float()
         token_type_ids = torch.zeros((tokenized.shape[0], tokenized.shape[1])).long()
-        
+
         if self.cuda:
             tokenized = tokenized.cuda()
             attention_mask = attention_mask.cuda()
             token_type_ids = token_type_ids.cuda()
-        
+
         with torch.no_grad():
-            classification_logits = self.net(tokenized, token_type_ids=token_type_ids, attention_mask=attention_mask, Q=None,\
-                                        e1_e2_start=e1_e2_start)
-            predicted = torch.softmax(classification_logits, dim=1).max(1)[1].item()
+            classification_logits = self.net(tokenized, token_type_ids=token_type_ids, attention_mask=attention_mask,
+                                             Q=None, \
+                                             e1_e2_start=e1_e2_start)
+            proba = torch.softmax(classification_logits, dim=1)
+        return proba
+
+    def infer_one_sentence(self, sentence):
+        proba = self.infer_one_sentence_proba(sentence)
+        predicted = proba.max(1)[1].item()
         print("Sentence: ", sentence)
         print("Predicted: ", self.rm.idx2rel[predicted].strip(), '\n')
         return predicted
